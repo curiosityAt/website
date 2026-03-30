4317d6a843626b9319c4390776a9ba4bf3d42276
Venkata Siva Surya Kolla <131696068+kvssJ@users.noreply.github.com>
2026-03-31 00:16:46 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: " google", tagId: 'G-9Y1SY26HZ2'
+      provider: "google", tagId: 'G-9Y1SY26HZ2'
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
